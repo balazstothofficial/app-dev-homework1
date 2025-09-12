eb1f6520a3bdcef87f6cf9c33edf2497f26faa0b
Balazs Toth <balazs.toth.isny@gmail.com>
2025-09-12 15:46:29 +0200

    Homework 1

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.2.10"
+}
+
+group = "app.dev.kotlin"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/main/kotlin/lecture/2Variables.kt
@@ -0,0 +1,13 @@
+package lecture
+
+/**
+ * Variables can be mutable or immutable.
+ *
+ * Tasks:
+ * - Find the difference!
+ *
+ * Resources: [Variables](https://kotlinlang.org/docs/basic-syntax.html#variables)
+ **/
+fun main() {
+
+}
